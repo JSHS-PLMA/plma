b6db35d73cb65ca609726b75684b4073ef29fff6
JSHS-PLMA <plma@jshsus.kr>
2025-04-26 03:20:30 +0900

    test

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # nvm 로드
 
 
-git remote set-url origin https://github.com/mineppl12/newplma.git
+git remote set-url origin https://github.com/JSHS-PLMA/plma.git
 git pull origin main 
 echo "깃 불러오기 끝"
 
 echo "npm 실행"
-nvm use 20.14.0
-node -v
 npm install 
 npm run build
 echo "빌드 끝"
